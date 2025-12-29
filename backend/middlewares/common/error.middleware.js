29127a6e9adc0c985387ba38fd745462ccc4b599
aswathi-123 <aswathiaswaa2@gmail.com>
2025-12-29 13:56:51 +0530

    feat: add .env.example file with environment variable placeholders

--- backend/middlewares/common/error.middleware.js
+++ backend/middlewares/common/error.middleware.js
@@ -1,9 +1,10 @@
 import logger from "../../config/logger.js";
+import { STATUS_CODES } from "../../utils/constants.js";
 
 export const errorHandler = (err, req, res, next) => {
   logger.error(err.message, { stack: err.stack });
 
-  res.status(err.status || 500).json({
+  res.status(err.status || STATUS_CODES.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: err.message || "Internal Server Error",
   });
